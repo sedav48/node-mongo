b4774c98d9ba1f290f2b8ffd8d51e771f2e27ec6
sedav48 <62444775+sedav48@users.noreply.github.com>
2022-07-09 13:03:16 -0400

    Wokshop 4

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ var logger = require('morgan');
 const passport = require('passport');
 const config = require('./config');
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -65,6 +68,7 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('./imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
